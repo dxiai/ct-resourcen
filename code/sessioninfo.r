e143cd7783b1367cd2c9105b001473f4f9cf9942
Christian Glahn <cpglahn@gmail.com>
2023-09-28 19:48:09 +0200

    more examples

--- code/sessioninfo.r
+++ code/sessioninfo.r
@@ -7,3 +7,11 @@ read_csv2("beispiele/Bestellungen_2.csv", locale = locale(decimal_mark = "."))
 library(jsonlite)
 read_file("beispiele/daten.json") |> fromJSON()
 read_json("beispiele/daten.json", simplifyVector = T)
+
+
+scan("beispiele/daten.json", what = character(), sep= "\n")
+readLines(con = file("beispiele/daten.json"), warn = F)
+read_lines("beispiele/daten.json")
+
+library(Matrix)
+Diagonal(4, 3:2)
